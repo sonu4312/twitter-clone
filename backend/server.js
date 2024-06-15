dc430588a4c039b566c66d6d38de20e3e11dc95e
sonu4312 <sonu.kushwaha9872@gmail.com>
2024-06-15 19:32:23 +0530

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ import dotenv from "dotenv";
 import connectionDB from "./db/connectionDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth-route.js";
 import userRoutes from "./routes/user-route.js";
 import postRoutes from "./routes/post-route.js";
-import notificationRoutes from "./routes/notification-route.js"
+import notificationRoutes from "./routes/notification-route.js";
 
 dotenv.config();
 
@@ -16,13 +17,15 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
-app.use(express.json({limit:"5mb"})); //it will parse req.body
+app.use(express.json({ limit: "5mb" })); //it will parse req.body
 app.use(express.urlencoded({ extended: true })); //to parse form data(ur;encoded)
 
 app.use(cookieParser()); //to parse cookies
@@ -32,6 +35,9 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 // app.get("/", (req, res) => {
 //   res.send("Server is ready");
 // });
